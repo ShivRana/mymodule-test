ec0d0b6e87484759d1030cb7f51d872c21fade9d
Shiv Rana <shivkumar.r@purplle.com>
2021-02-22 23:38:23 +0530

    first commit

---
+++ src/main/java/com/wavtekmedia/mylibrary_test/Logger.java
@@ -0,0 +1,9 @@
+package com.wavtekmedia.mylibrary_test;
+
+import android.util.Log;
+
+public class Logger {
+    public static void showLog(String message) {
+        Log.d("Tag", message);
+    }
+}
